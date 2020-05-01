3a8837ca824d362636932245b626bcc1d28035f6
Wesley <wesleyhokage123@outlook.com>
2020-05-01 06:14:37 -0300

    Atualizações
    Animação e funcionalidades de pulo e correr;
    
    Funcionlidade da mascara;
    
    Scripts das respectivas coisas citadas a cima;
    
    implementação de um ground temporario

---
+++ _MiniJamOasis/Assets/Scripts/CreationOasis.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationOasis : MonoBehaviour
+{
+    public Transform Center;
+
+    public SpriteMask mask;
+    public GameObject Oasis;
+    void Awake(){
+        mask.enabled = false;
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        maskCreating();
+        maskDisable();
+    }
+    void maskCreating(){
+        if(Input.GetButtonDown("Fire1") && mask.enabled == false){
+            Instantiate(Oasis, new Vector3(Center.transform.position.x,Center.transform.position.y,Center.transform.position.z), Center.transform.rotation);
+           Oasis.SetActive(true);
+           mask.enabled = true;
+           Time.timeScale = 0.6f;
+           Player.instance.speed = 0;
+           Player.instance.JumpForce = 0;
+        }
+    }
+    void maskDisable(){
+        if(Input.GetKeyDown(KeyCode.Mouse1) && mask.enabled == true){
+                Oasis.SetActive(false);
+                mask.enabled = false;
+                Time.timeScale = 1f;
+                Player.instance.speed = Player.instance.speedNormal;
+                Player.instance.JumpForce = Player.instance.JumpForceNormal ;
+
+            }
+        }
+}
---
+++ _MiniJamOasis/Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+        public float speed, speedNormal;
+        public float JumpForce,JumpForceNormal;
+
+        private bool isJumping;
+
+        private SpriteRenderer _sr;
+        private Rigidbody2D _rb;
+        private Animator _anim;
+   void Awake(){
+       _rb = GetComponent<Rigidbody2D>();
+       _sr = GetComponent<SpriteRenderer>();
+       _anim = GetComponent<Animator>();
+   }
+    void Start()
+    {
+        instance = this;
+    }
+    void Update()
+    {
+        move();
+        Jump();
+    }
+    void move(){
+        Vector3 movement = new  Vector3(Input.GetAxis("Horizontal"),0,0);
+        transform.position += movement * speed * Time.deltaTime;
+        if(Input.GetAxis("Horizontal") > 0.1f){
+            _anim.SetBool("Run", true);
+             _anim.SetBool("Idle", false);
+            _sr.flipX = false;
+        }
+        else if(Input.GetAxis("Horizontal") < -0.1f){
+            _anim.SetBool("Run", true);
+            _anim.SetBool("Idle", false);
+            _sr.flipX = true;
+        }
+        else {
+            _anim.SetBool("Idle", true);
+             _anim.SetBool("Run", false);
+            _sr.flipX = false;
+        }
+    }
+    void Jump(){
+        if(Input.GetButton("Jump") && !isJumping){
+            _rb.AddForce(new Vector2(0,JumpForce * Time.deltaTime), ForceMode2D.Impulse);
+            _anim.SetTrigger("Jump");
+             _anim.SetBool("Idle", false);
+              _anim.SetBool("Run", false);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D outro){
+        if(outro.gameObject.layer == 8){
+            isJumping = false;
+        }
+    }
+    void OnCollisionExit2D(Collision2D outro){
+        if(outro.gameObject.layer == 8){
+            isJumping = true;
+        }
+    }
+}
